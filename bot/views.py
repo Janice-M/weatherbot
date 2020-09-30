9f4211dcdb673f8639eb711785c3d20d10417837
Janice-M <janiceink001@gmail.com>
2020-10-01 00:28:06 +0300

    views

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+@csrf_exempt
+def webhook(request):
+    response = MessagingResponse()
+    if request.method == "POST":
+        message = request.POST.get("Body")
+        print(message)
+        response.message('You said: ' + message)
+    return HttpResponse(response.to_xml(), content_type='text/xml')
