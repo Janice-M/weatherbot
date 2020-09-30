cfff44b0bb5756881d5135e6df66b8b680f9bc32
Janice-M <janiceink001@gmail.com>
2020-10-01 00:40:57 +0300

    get the motherfucking weather bitch

---
+++ bot/query_weather.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def get_weather(lat, lon, api_key):
+    weather_response = requests.get(
+        f"https://api.openweathermap.org/data/2.5/onecall?lat"
+        f"={lat}&lon={lon}&exclude=hourly&appid={api_key}&"
+        f"units=metric").json()
+    return weather_response
