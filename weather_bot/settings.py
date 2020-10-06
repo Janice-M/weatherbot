79466727e7a0084437d5cf297843ce373c0befa8
Janice-M <janiceink001@gmail.com>
2020-10-06 21:37:24 +0300

    open weather key

--- weather_bot/settings.py
+++ weather_bot/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 
 from pathlib import Path
-
+OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
