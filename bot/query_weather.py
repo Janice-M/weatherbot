2deb3c810cee6ee9d472fe5eae6289fd2923fb70
Janice-M <janiceink001@gmail.com>
2020-10-07 23:52:07 +0300

    current weather

--- bot/query_weather.py
+++ bot/query_weather.py
@@ -10,3 +10,15 @@ def get_weather(lat, lon, api_key):
         f"={lat}&lon={lon}&exclude=hourly&appid={api_key}&"
         f"units=metric").json()
     return weather_response
+
+def generate_weather_message(weather_response):
+    current_weather = {
+        "temperature": int(weather_response["current"]["temp"]),
+        "day_of_week": calendar.day_name[datetime.fromtimestamp(
+            weather_response["current"]["dt"]).weekday()],
+        "description":
+            weather_response["current"]["weather"][0]["description"],
+        "main":
+            weather_response["current"]["weather"][0]["main"]
+
+    }
